460547de17af47e4c1e9f36c308a0bc22c0222d1
Pranav Rana <pranavrana1258@gmail.com>
2026-04-03 20:14:27 -0400

    Set up Prisma with PostgreSQL, add user model and GraphQL API for user creation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const PORT = Number(process.env.PORT) || 8000;
-app.get('/', (req, res) => {
-    res.json({ message: 'Server is up and running!' });
-});
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-//# sourceMappingURL=index.js.map
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@as-integrations/express5';
+async function init() {
+    const app = express();
+    const PORT = Number(process.env.PORT) || 8000;
+    app.use(express.json());
+    //Create graphql server
+    const gqlServer = new ApolloServer({
+        typeDefs: `
+        type Query {
+            hello: String
+        }`,
+        resolvers: {
+            Query: {
+                hello: () => 'Hello, world!'
+            }
+        }
+    });
+    //Start gql server
+    await gqlServer.start();
+    app.get('/', (req, res) => {
+        res.json({ message: 'Server is up and running!' });
+    });
+    app.use('/graphql', expressMiddleware(gqlServer));
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+init();
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
 import express from 'express';
+import 'dotenv/config'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
-
+import { prismaClient } from './lib/db';
 async function init(){
     const app = express();
     const PORT = Number(process.env.PORT) || 8000
-
+    
     app.use(express.json());
     //Create graphql server
     const gqlServer = new ApolloServer({
         typeDefs: `
-        type Query {
-            hello: String
-        }`,
+            type Query {
+                hello: String
+            }
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+            }`,
         resolvers: {
             Query: {
                 hello: () => 'Hello, world!'
+            },
+            Mutation: {
+                createUser: async(_, {firstName, lastName, email, password}: 
+                    {firstName: string; lastName: string; email: string; password: string;}) => {
+                        await prismaClient.user.create({
+                            data: {
+                                firstName,
+                                lastName,
+                                email,
+                                password,
+                                salt: "random_salt" // In a real application, you should generate a unique salt for each user and hash the password
+                            }
+                        })
+                        return true;
+                    }
             }
         }
     })
