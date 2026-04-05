2c7aba469a51c382e18bfe55202363994973a678
Pranav Rana <pranavrana1258@gmail.com>
2026-04-05 19:06:25 -0400

    Fix formatting issues in GraphQL type definitions and queries; improve code readability in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,37 @@
-import express from 'express';
-import 'dotenv/config'
-import { expressMiddleware } from '@as-integrations/express5';
-import createApolloGraphqlServer from './graphql';
-import UserService from './service/user';
+import express from "express";
+import "dotenv/config";
+import { expressMiddleware } from "@as-integrations/express5";
+import createApolloGraphqlServer from "./graphql";
+import UserService from "./service/user";
 
-async function init(){
-    const app = express();
-    const PORT = Number(process.env.PORT) || 8000
-    
-    app.use(express.json());
+async function init() {
+  const app = express();
+  const PORT = Number(process.env.PORT) || 8000;
 
-    app.get('/', (req, res) => {
-    res.json({message: 'Server is up and running!'});
-    });
+  app.use(express.json());
 
-    app.use('/graphql', expressMiddleware(await createApolloGraphqlServer(), {
-        context: async ({ req }) => {
-          const token = req.headers.authorization;
-          try {
-            const decodedToken = UserService.decodeToken(token as string);
-            return {user: decodedToken};
-          } catch (error) {
-            console.error("Error verifying token:", error);
-          }
-        },
-      }));
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running!" });
+  });
 
-    app.listen(PORT, () => {
+  app.use(
+    "/graphql",
+    expressMiddleware(await createApolloGraphqlServer(), {
+      context: async ({ req }) => {
+        const token = req.headers.authorization;
+        try {
+          const decodedToken = UserService.decodeToken(token as string);
+          return { user: decodedToken };
+        } catch (error) {
+          console.error("Error verifying token:", error);
+        }
+      },
+    })
+  );
+
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    });
+  });
 }
 
 init();
